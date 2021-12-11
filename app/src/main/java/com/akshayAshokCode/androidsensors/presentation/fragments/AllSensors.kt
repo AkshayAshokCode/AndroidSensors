ea4633f2ce066006dc89fb7440fde30e5daa8f1c
Riderpool1 <akshayashokan1054.com>
2021-12-11 11:00:04 +0530

    proper architecture

--- app/src/main/java/com/akshayAshokCode/androidsensors/presentation/fragments/AllSensors.kt
+++ app/src/main/java/com/akshayAshokCode/androidsensors/presentation/fragments/AllSensors.kt
@@ -11,7 +11,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.akshayAshokCode.androidsensors.Constants
 import com.akshayAshokCode.androidsensors.R
 import com.akshayAshokCode.androidsensors.adapter.SensorAdapter
 import com.akshayAshokCode.androidsensors.data.SensorModel
@@ -30,11 +34,24 @@ class AllSensors : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.all_sensors, container, false)
         binding.recyclerview.layoutManager = GridLayoutManager(context, 2)
-        allSensorsViewModel=ViewModelProvider(this).get(AllSensorsViewModel::class.java)
-        allSensorsViewModel.sensors.observe(viewLifecycleOwner, { sensor->
-            sensorAdapter = SensorAdapter(context, sensor)
+        allSensorsViewModel = ViewModelProvider(this).get(AllSensorsViewModel::class.java)
+        allSensorsViewModel.sensors.observe(viewLifecycleOwner, { sensor ->
+            sensorAdapter = SensorAdapter(context, sensor) { selectedItem: SensorModel ->
+                clickedSensor(selectedItem)
+            }
             binding.recyclerview.adapter = sensorAdapter
         })
         return binding.root
+    }
+
+    private fun clickedSensor(sensor: SensorModel) {
+        when(sensor.id){
+            Constants.METAL_DETECTOR-> findNavController().navigate(R.id.action_allSensors_to_metalDetector)
+            Constants.GRAVITY_METER-> findNavController().navigate(R.id.action_allSensors_to_gravityMeter)
+            Constants.HEART_RATE_METER-> findNavController().navigate(R.id.action_allSensors_to_heartRateMeter)
+            Constants.PRESSURE_METER-> findNavController().navigate(R.id.action_allSensors_to_pressureMeter)
+            Constants.RELATIVE_HUMIDITY-> findNavController().navigate(R.id.action_allSensors_to_relativeHumidityMeter2)
+        }
+        //To handle update on clicks
     }
 }
